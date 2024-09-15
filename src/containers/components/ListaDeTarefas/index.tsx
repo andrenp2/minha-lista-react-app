dcbdb1319220c7f05455b460b73650a00476a1e3
Andre Pavan <dehnuernberg@hotmail.com>
2024-09-15 11:23:17 -0300

    adicionando parametrizacao para as tags e botoes

--- src/containers/components/ListaDeTarefas/index.tsx
+++ src/containers/components/ListaDeTarefas/index.tsx
@@ -1,25 +1,41 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ver aula 3 da EBAC',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Finalizar o projeto React',
+    descricao: 'Finalizar as aulas do projeto 5',
+    prioridade: 'urgente',
+    status: 'concluída'
+  },
+  {
+    titulo: 'Estudar o desafio técnico',
+    descricao: 'Rever os códigos do desafio',
+    prioridade: 'importante',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
--- src/containers/components/Tarefa/index.tsx
+++ src/containers/components/Tarefa/index.tsx
@@ -1,16 +1,39 @@
 import * as S from './styles'
+import { useState } from 'react'
 
-const Tarefa = () => (
-  <S.Card>
-    <S.Titulo>Nome da tarefa</S.Titulo>
-    <S.Tag>importante</S.Tag>
-    <S.Tag>pendente</S.Tag>
-    <S.Descricao />
-    <S.BarraAcoes>
-      <S.Botao>Editar</S.Botao>
-      <S.Botao>Remover</S.Botao>
-    </S.BarraAcoes>
-  </S.Card>
-)
+type Props = {
+  titulo: string
+  prioridade: string
+  status: string
+  descricao: string
+}
+
+const Tarefa = ({ descricao, prioridade, status, titulo }: Props) => {
+  const [estaEditando, setEstaEditando] = useState(false)
+
+  return (
+    <S.Card>
+      <S.Titulo>{titulo}</S.Titulo>
+      <S.Tag prioridade={prioridade}>{prioridade}</S.Tag>
+      <S.Tag status={status}>{status}</S.Tag>
+      <S.Descricao value={descricao} />
+      <S.BarraAcoes>
+        {estaEditando ? (
+          <>
+            <S.BotaoSalvar>Salvar</S.BotaoSalvar>
+            <S.BotaoCanclarRemover onClick={() => setEstaEditando(false)}>
+              Cancelar
+            </S.BotaoCanclarRemover>
+          </>
+        ) : (
+          <>
+            <S.Botao onClick={() => setEstaEditando(true)}>Editar</S.Botao>
+            <S.BotaoCanclarRemover>Remover</S.BotaoCanclarRemover>
+          </>
+        )}
+      </S.BarraAcoes>
+    </S.Card>
+  )
+}
 
 export default Tarefa
